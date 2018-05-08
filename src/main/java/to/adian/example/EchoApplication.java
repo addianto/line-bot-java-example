4de6f74f8d129fcdcc18c8495be110ab43a90149
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-05-08 22:00:28 +0700

    Initial commit

---
+++ src/main/java/to/adian/example/EchoApplication.java
@@ -0,0 +1,30 @@
+package to.adian.example;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@LineMessageHandler
+public class EchoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoApplication.class, args);
+    }
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("Event: " + event);
+        return new TextMessage("You sent: " + event.getMessage());
+    }
+
+    @EventMapping
+    public void handleDefaultMessage(Event event) {
+        System.out.println("Event: " + event);
+    }
+}
